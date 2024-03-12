7ce684979b1cd9d0bb43a15d8748dbfa1fd6b140
fardosa001 <fardosaadow@gmail.com>
2024-03-12 22:37:05 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,10 @@
 export default function cleanSet(set, startString) {
+  const filteredValues = [...set].filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
   if (startString === '') {
     return '';
   }
-  const filteredValues = [...set].filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
-    // Join the filtered values into a string separated by '-'
+  // Join the filtered values into a string separated by '-'
   return filteredValues.join('-');
 }
