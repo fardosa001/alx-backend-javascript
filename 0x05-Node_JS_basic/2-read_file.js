b24c87eb4bd0c1f6a771c2d957eca57f8c08ee7e
fardosa001 <fardosaadow@gmail.com>
2024-05-03 17:13:18 +0000

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const countStudents = (aPath) => {
+const countStudents = (Path) => {
   try {
-    let data = fs.readFileSync(aPath, 'utf8').toString().split('\n');
+    let data = fs.readFileSync(Path, 'utf8').toString().split('\n');
     data = data.slice(1, data.length - 1);
     console.log(`Number of students: ${data.length}`);
     const subjects = {};
