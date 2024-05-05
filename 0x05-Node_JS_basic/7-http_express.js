c12cef803d6668048cd06bfa1d9f3dde484c515f
fardosa001 <fardosaadow@gmail.com>
2024-05-05 17:49:31 +0000

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,16 +2,17 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
-app.get('/students', async (req, res) => {
-  const title = 'This is the list of our students\n';
-  try {
-    const data = await countStudents(process.argv[2]);
-    res.send(`${title}${data.join('\n')}`);
-  } catch (error) {
-    res.send(`${title}${error.message}`);
-  }
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2].toString()).then((output) => {
+    res.send(['This is the list of our students', output].join('\n'));
+  }).catch(() => {
+    res.send('This is the list of our students\nCannot load the database');
+  });
 });
 
 app.listen(1245);
+
 module.exports = app;
